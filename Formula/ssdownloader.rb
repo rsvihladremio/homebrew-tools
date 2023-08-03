40327af5b289e7045f3080b6bed22927abf86bce
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-08-03 11:26:59 +0200

    updating ssdownloader

--- Formula/ssdownloader.rb
+++ Formula/ssdownloader.rb
@@ -15,12 +15,12 @@ class Ssdownloader < Formula
 
   desc "ssdownloader integrates Zendesk and SendSafely APIs to easily download tickets@"
   homepage "https://github.com/rsvihladremio/ssdownloader"
-  url "https://github.com/rsvihladremio/ssdownloader/archive/v0.4.4.tar.gz"
-  sha256 "aa84ff101a5a7129e9aec674b77c59fc49544c6ff2ef4d58cb64c65098ad1b00"
+  url "https://github.com/rsvihladremio/ssdownloader/archive/v0.4.5.tar.gz"
+  sha256 "66b7bcdcbd290362e20c9327bb225dd85e2552fff0c4ac5151a6f1695a1ef8dd"
   license "Apache-2.0"
   depends_on "go@1.20"
   def install
-    system "go build -ldflags '-X github.com/rsvihladremio/ssdownloader/cmd.GitSha=9cb08a6 -X github.com/rsvihladremio/ssdownloader/cmd.Version=v0.4.4' -o ./bin/ssdownloader"
+    system "go build -ldflags '-X github.com/rsvihladremio/ssdownloader/cmd.GitSha=42ffd8c -X github.com/rsvihladremio/ssdownloader/cmd.Version=v0.4.5' -o ./bin/ssdownloader"
     bin.install "./bin/ssdownloader" => "ssdownloader"
   end
   test do
