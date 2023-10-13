9e5b42b932e4ffd0a34e229e57577ab264d68009
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-10-13 15:21:45 +0200

    updated dremio-stress version to v0.3.0

--- Formula/dremio-stress.rb
+++ Formula/dremio-stress.rb
@@ -1,11 +1,11 @@
 class DremioStress < Formula
   desc "Simple tool to stress dremio via ODBC and REST interfaces"
   homepage "https://github.com/rsvihladremio/dremio-stress"
-  version "0.2.1"
+  version "0.3.0"
 
   # Define the download URLs and corresponding SHA256 checksums for the binary releases
   url "https://github.com/rsvihladremio/dremio-stress/releases/download/v#{version}/dremio-stress-mac-amd64.zip"
-  sha256 "21e7224392725ea1479ddc9175b0f44edcdb4c6bed96bea7d6b5e736da8207d2"
+  sha256 "cbde2aa190c4e3a54f127e7fa169a0c60887d49df88a6980458850fbf1d8ba8c"
 
   def install
     bin.install "dremio-stress" => "dremio-stress"
