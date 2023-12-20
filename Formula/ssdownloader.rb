08c7ad0f11d6866572966675662fbc9aea0c2acf
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-12-20 14:50:34 +0100

    ssdownloader binaries and 0.4.10

--- Formula/ssdownloader.rb
+++ Formula/ssdownloader.rb
@@ -15,15 +15,42 @@ class Ssdownloader < Formula
 
   desc "ssdownloader integrates Zendesk and SendSafely APIs to easily download tickets@"
   homepage "https://github.com/rsvihladremio/ssdownloader"
-  url "https://github.com/rsvihladremio/ssdownloader/archive/v0.4.9.tar.gz"
-  sha256 "4061af49773f929d7924e20b6074797e39c749da55ee20bec56af226b750ec51"
   license "Apache-2.0"
-  depends_on "go@1.21"
+  version "0.4.10"
+  on_macos do
+
+    if Hardware::CPU.intel?
+      # Define the download URLs and corresponding SHA256 checksums for the binary releases
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-darwin-amd64.zip"
+      sha256 "b2860d395a9077a3835ef87ba78e6f57a2b0e8d795d3466dafd7b436a0051d2a"
+    end
+
+    if Hardware::CPU.arm?
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-darwin-arm64.zip"
+      sha256 "3f055824d261bc4bfc9350b707f444545f398aa9a3d2113fa9db6573fd85ffd9"
+    end
+  end
+
+  on_linux do
+
+    if Hardware::CPU.intel?
+      # Define the download URLs and corresponding SHA256 checksums for the binary releases
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-linux-amd64.zip"
+      sha256 "16b253d52722ee70850b9b95528c61375df5af23017628fb06def489770c9672"
+    end
+
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-linux-arm64.zip"
+      sha256 "bbaeb3a595a5e3406f61ae7d4c50d4360d0fba249db88afa15b58fd3e48a6979"
+    end
+  end
 
   def install
-    system "go build -ldflags '-X github.com/rsvihladremio/ssdownloader/cmd.GitSha=b4f8031 -X github.com/rsvihladremio/ssdownloader/cmd.Version=v0.4.9' -o ./bin/ssdownloader"
-    bin.install "./bin/ssdownloader" => "ssdownloader"
+    bin.install "ssdownloader" => "ssdownloader"
   end
   test do
+    # Add test logic here if applicable
+    system "#{bin}/ssdownloader", "version"
   end
+
 end
