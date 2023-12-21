1c6959362098042f4d597f489542ac34999aa997
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2023-12-21 22:53:28 +0100

    bumping ddc to 0.8.3

--- Formula/ddc.rb
+++ Formula/ddc.rb
@@ -1,19 +1,19 @@
 class Ddc < Formula
     desc "collect logs of Dremio for analysis"
     homepage "https://github.com/dremio/dremio-diagnostic-collector"
-    version "0.7.3"
+    version "0.8.3"
 
     on_macos do
 
       if Hardware::CPU.intel?
         # Define the download URLs and corresponding SHA256 checksums for the binary releases
         url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v#{version}/ddc-mac-intel.zip"
-        sha256 "dca14af7fbd7dd113d414b14e1e10ddb92706966ac25e4d2be0d5f608050400e"
+        sha256 "b5544b5f7e93435a2116861c9f585674c8ce2659467640fc2f92bc3b0b358354"
       end
 
       if Hardware::CPU.arm?
           url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v#{version}/ddc-mac-m-series.zip"
-          sha256 "5c2f5f8073fa0c4fa875c1ddf23f59161a9cc68f62d87a1a9290d9a7f4f50e6f"
+          sha256 "6501f918f03baa3441fef92bb5ba7d5f5d54d120e95352b88ce3a459f2b244df"
       end
     end
 
@@ -22,12 +22,12 @@ class Ddc < Formula
       if Hardware::CPU.intel?
         # Define the download URLs and corresponding SHA256 checksums for the binary releases
         url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v#{version}/ddc-linux-amd64.zip"
-        sha256 "b5f3372bd2e4641e4917bad63dba41cac0d8495cbf83622ef3bc53fa30816dd0"
+        sha256 "a42cf1b4a53e3d57e21dc16eb8e8ae113eae5803b1490d8d5d4173076eaae9b8"
       end
 
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
           url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v#{version}/ddc-linux-arm64.zip"
-          sha256 "5545fe8c674c8f4f3311ee8523db8e9759bdeb515fd0a3aabcf0b2f07e0a5f7d"
+          sha256 "28e13ca44480c84996147d588bdecde30cb23f18dc8c83459012605db635b03a"
       end
 
 
@@ -40,4 +40,4 @@ class Ddc < Formula
       # Add test logic here if applicable
       system "#{bin}/ddc", "version"
     end
-  end
+end
