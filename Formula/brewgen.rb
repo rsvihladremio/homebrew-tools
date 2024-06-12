3f033148f26e5d5358a32a9436dc3f477b177286
Ryan SVIHLA <105286284+rsvihladremio@users.noreply.github.com>
2024-06-12 20:29:51 +0200

    new shas

--- Formula/brewgen.rb
+++ Formula/brewgen.rb
@@ -7,12 +7,12 @@ class Brewgen < Formula
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
       url "https://github.com/rsvihladremio/brewgen/releases/download/0.1.0/brewgen-0.1.0-amd64-apple-darwin.zip"
-      sha256 "fe8e21164f8daeb51b4b8ec1d3d9462bf094f18d37b58179656510de4c63547f"
+      sha256 "b65f28ebd24e5da8cc9fe8ac52b270d75f198516ce08f14b9004a0c519a1b7b2"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/rsvihladremio/brewgen/releases/download/0.1.0/brewgen-0.1.0-arm64-apple-darwin.zip"
-      sha256 "053fbf08927919ca11df463808b40a0b79e87ada7ee6b4a7ad999657b4dba60d"
+      sha256 "fa81f13c98dcbf948a8840c2a33e0e25707b141c3cf5685f9a6919e061660372"
     end
   end
 
@@ -20,12 +20,12 @@ class Brewgen < Formula
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
       url "https://github.com/rsvihladremio/brewgen/releases/download/0.1.0/brewgen-0.1.0-amd64-unknown-linux-gnu.zip"
-      sha256 "54b0b0b5895f7607a09185ce060d83c9f74843f714abe7eecb335f6193bfd45d"
+      sha256 "1ec42485cba72d700d697ab049679119411e3933fd42d382103eeee91ff0d08c"
     end
   end
 
   def install
-    libexec.install "brewgen" => "brewgen"
+    libexec.install "bin/brewgen" => "brewgen"
     bin.write_exec_script libexec/"brewgen"
   end
 
