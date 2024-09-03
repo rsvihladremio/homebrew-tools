8960b063e3b62bae77265b0bc44748ed5bdab715
Ryan SVIHLA <105286284+rsvihladremio@users.noreply.github.com>
2024-09-03 16:13:33 +0200

    updated dqd

--- Formula/dqd.rb
+++ Formula/dqd.rb
@@ -1,41 +1,41 @@
 class Dqd < Formula
   desc "identifies problems in dremio"
   homepage "https://github.com/rsvihladremio/dqd"
-  version "0.11.4"
+  version "0.12.0"
 
   on_macos do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/dqd/releases/download/v0.11.4/dqd-0.11.4-mac-intel.zip"
-      sha256 "d2290934012829b1d4a6487aabc8b437e0386533ed30df7fde47b355f52af4fe"
+      url "https://github.com/rsvihladremio/dqd/releases/download/v0.12.0/dqd-0.12.0-mac-intel.zip"
+      sha256 "39a3cd1fcd272a3d8ace24d8ca0776e59ef561447b45434a228eea37872ed526"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/rsvihladremio/dqd/releases/download/v0.11.4/dqd-0.11.4-mac-apple-silicon.zip"
-      sha256 "0df01e581e105a0b821d10078083f2bcb0245337141b4e8f52617533076a7b20"
+      url "https://github.com/rsvihladremio/dqd/releases/download/v0.12.0/dqd-0.12.0-mac-apple-silicon.zip"
+      sha256 "3f7276cc5589e2b6a5d7cbb81dd65aab4622ff9770d6378c8b1aae392b71fbe4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/dqd/releases/download/v0.11.4/dqd-0.11.4-linux-amd64.zip"
-      sha256 "dff5c6a133724682b5c9adcbeedeb6684a5e487e7f483314a85f2589a704a6e7"
+      url "https://github.com/rsvihladremio/dqd/releases/download/v0.12.0/dqd-0.12.0-linux-amd64.zip"
+      sha256 "b932745e9803f506818c80de11c39ff65f49c0b3b263967ba277e8e5e91d81d5"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rsvihladremio/dqd/releases/download/v0.11.4/dqd-0.11.4-linux-arm64.zip"
-      sha256 "52e0239c369a78cc5c7db7a0dc6ed31ea822a083ee98a83deb39d664c73ab6d0"
+      url ""
+      sha256 ""
     end
   end
 
   def install
-    libexec.install "dqd" => "dqd"
+    libexec.install "bin/dqd" => "dqd"
     bin.write_exec_script libexec/"dqd"
   end
 
   test do
     # Add test logic here if applicable
-    system "#{bin}/dqd", "help"
+    system "#{bin}/dqd", "version"
   end
 end
