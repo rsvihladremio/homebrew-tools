7a297de1839293807825088069a25e94c781266b
Ryan SVIHLA <105286284+rsvihladremio@users.noreply.github.com>
2024-12-18 18:18:38 +0100

    upgrade of ddc

--- Formula/ddc.rb
+++ Formula/ddc.rb
@@ -1,31 +1,31 @@
 class Ddc < Formula
-  desc "capturing logs and diags for dremio"
+  desc "collect logs and metrics from dremio"
   homepage "https://github.com/dremio/dremio-diagnostic-collector"
-  version "3.2.7"
+  version "3.2.8"
 
   on_macos do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.7/ddc-mac-intel.zip"
-      sha256 "3129e7b4e7ef3bedc010b8613843de5cab1de43b49c0e44fb259bc18e276bb67"
+      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.8/ddc-mac-intel.zip"
+      sha256 "2eb95cff73e6e6d4ed6e51eab09d3db0a9ddbe248a5780c4b703e35ac92f855c"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.7/ddc-mac-m-series.zip"
-      sha256 "f1009fdc4c89d56d88be174f5f9319702e080190e46764c0fb4f1e55451819ea"
+      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.8/ddc-mac-m-series.zip"
+      sha256 "2f45406c58aaf1072d902dd2be1fef50e7a4ecc732861e725bc3c1a5b150565b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.7/ddc-linux-amd64.zip"
-      sha256 "6ada9b5342780d88074f1be00be4a3a408f20513a2ef15725b8d51b24464a6f2"
+      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.8/ddc-linux-amd64.zip"
+      sha256 "31e9802779e1f9f1fe9181973287195f7e00f576fc6310e944e3e35cef450d39"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.7/ddc-linux-arm64.zip"
-      sha256 "e8e0ecfc9ad7bee7cdb95cef0603084a15cc1de8f2be7761ed7c8b6f26901fb1"
+      url "https://github.com/dremio/dremio-diagnostic-collector/releases/download/v3.2.8/ddc-linux-arm64.zip"
+      sha256 "74d436deb3dace15b6a3142b3768e130cb32f5221b4d9e5bc1cb393b5142edff"
     end
   end
 
