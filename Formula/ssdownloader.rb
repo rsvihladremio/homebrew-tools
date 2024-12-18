804c2fd4153e3bf9e597302429b83cb735d9e401
Ryan SVIHLA <105286284+rsvihladremio@users.noreply.github.com>
2024-12-18 10:21:03 +0100

    ssdownloader upgrade

--- Formula/ssdownloader.rb
+++ Formula/ssdownloader.rb
@@ -1,56 +1,41 @@
-#  Copyright 2022 Ryan Svihla
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 class Ssdownloader < Formula
-
-  desc "ssdownloader integrates Zendesk and SendSafely APIs to easily download tickets@"
+  desc "download files from zendesk tickets and sendsafely links"
   homepage "https://github.com/rsvihladremio/ssdownloader"
-  license "Apache-2.0"
-  version "0.4.11"
-  on_macos do
+  version "0.4.12"
 
+  on_macos do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-darwin-amd64.zip"
-      sha256 "de65ef5e3fec99b83e369770ce60e1458b7e4d4394f0c653ed8fc0ba603fd6ce"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.12/ssdownloader-darwin-amd64.zip"
+      sha256 "189c8603427dc18e21e52600da094cb43aa616ed2b4a473131cf3178c45b5f73"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-darwin-arm64.zip"
-      sha256 "0b312f43eac0ba88adebb8731bfbb9084d7d69281b9afa77479ce1c5d5538e6a"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.12/ssdownloader-darwin-arm64.zip"
+      sha256 "225d811cc01a6ceb5bde1bfca596c3831f9f778a3980c48a27b526763f989a8d"
     end
   end
 
   on_linux do
-
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-linux-amd64.zip"
-      sha256 "583a4072752c67ee2e72d5ddb199c3166a81335cbc36adbda3faf9d98eddb324"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.12/ssdownloader-linux-amd64.zip"
+      sha256 "94d9bd5e36e619035a036399d99d5628e95a303bb55cee23ebaf78e0a9cfc6ba"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v#{version}/ssdownloader-linux-arm64.zip"
-      sha256 "178b9394948502bc35dcbcbfbe5134b98803ca376b3ac0d25b41b695982fc1f5"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.12/ssdownloader-linux-arm64.zip"
+      sha256 "cf31f2b30ffb116fe5f30f1ca2e26d50c581a30348cce6fe4f8b56bee16c2bc9"
     end
   end
 
   def install
-    bin.install "ssdownloader" => "ssdownloader"
+    libexec.install "bin/ssdownloader" => "ssdownloader"
+    bin.write_exec_script libexec/"ssdownloader"
   end
+
   test do
     # Add test logic here if applicable
     system "#{bin}/ssdownloader", "version"
   end
-
 end
