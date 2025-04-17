978b7013da7ee5232fc9d833a4a16bab643d9c44
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2025-04-17 20:44:48 +0200

    ssdownloader 0.4.14

--- Formula/ssdownloader.rb
+++ Formula/ssdownloader.rb
@@ -1,31 +1,31 @@
 class Ssdownloader < Formula
   desc "retrieves all SendSafely uploads to a ticket"
   homepage "https://github.com/rsvihladremio/ssdownloader"
-  version "0.4.13"
+  version "0.4.14"
 
   on_macos do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.13/ssdownloader-darwin-amd64.zip"
-      sha256 "c7d2691f3d25fc3e8b91217dff14ac3dd934d764978d2496ea7ed0f00ac04988"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.14/ssdownloader-darwin-amd64.zip"
+      sha256 "7601e4e9018b29d7341c382e30dc1bc225d58870158611bf468e2d69345c2b2d"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.13/ssdownloader-darwin-arm64.zip"
-      sha256 "3959086a446a010e92f48b14c83822eb622169c7ff0e3f45bb599e488e857679"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.14/ssdownloader-darwin-arm64.zip"
+      sha256 "db9b09d9c096af03ecf33fc08bfd933ecb2630bf050e4205cb856d420a73562e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       # Define the download URLs and corresponding SHA256 checksums for the binary releases
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.13/ssdownloader-linux-amd64.zip"
-      sha256 "3a12a12bf36a2e54ce9d26dde7b85a5c2ca59cc1f7152039cf1d4f0f62aef861"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.14/ssdownloader-linux-amd64.zip"
+      sha256 "9ef9a7b9ac324d246f096556f4b0aac97ef8a38b11085e298d96637e866d0f40"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.13/ssdownloader-linux-arm64.zip"
-      sha256 "220b590b0cd2174d5bc9b779eea4d60ffda2fd524c81ecef5ad4d765bc68f99d"
+      url "https://github.com/rsvihladremio/ssdownloader/releases/download/v0.4.14/ssdownloader-linux-arm64.zip"
+      sha256 "f886da877506171a0acbe1792ad22063238993b1601960fd66056d31a90985f7"
     end
   end
 
